b896537a6daf724423b109c4d26bc02a904a542a
Tiyan <sulistiyanto404@gmail.com>
2023-06-11 07:59:10 +0700

    update variable name

--- app/src/main/java/com/example/bottomnav/MainActivity.kt
+++ app/src/main/java/com/example/bottomnav/MainActivity.kt
@@ -12,7 +12,7 @@ import com.example.bottomnav.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    var name: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
